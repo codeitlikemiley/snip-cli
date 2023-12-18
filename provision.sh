fc890c6aaf39bd24b5e6a65fb93c320558d641c8
Uriah <codeitlikemiley@gmail.com>
2023-12-18 23:03:24 +0800

    provision script uses zigbuild

--- provision.sh
+++ provision.sh
@@ -2,6 +2,6 @@
 
 cargo clean
 rm snip.pkg
-cargo build --release
+cargo zigbuild --release
 cargo bundle --release
-pkgbuild --root ./target/release/bundle/osx/Snip.app --install-location "/Applications/Snip.app" --identifier com.codeitlikemiley.snip --version 0.1.0 --scripts ./scripts snip.pkg
+pkgbuild --root ./target/release/bundle/osx/snip.app --install-location "/Applications/snip.app" --identifier com.codeitlikemiley.snip --version 0.1.0 --scripts ./scripts snip.pkg
